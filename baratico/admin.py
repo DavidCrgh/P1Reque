a983aa95d86388fef99d5fbd2626e24718525991
DavidCRgh <dval7788@gmail.com>
2018-03-29 17:21:16 -0600

    * Agregado modelo de administracion de producto en baratico/admin.py.

--- baratico/admin.py
+++ baratico/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Categoria, Producto
 
 # Register your models here.
 
@@ -15,5 +15,11 @@ class CategoriaAdmin(admin.ModelAdmin):
         model = Categoria
 
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'cantidad', 'precio', 'categoria']
+    list_filter = ['categoria']
+    search_fields = ['nombre']
+
+
 admin.site.register(Categoria, CategoriaAdmin)
-admin.site.register(Producto)
+admin.site.register(Producto, ProductoAdmin)
