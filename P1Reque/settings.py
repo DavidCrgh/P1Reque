d6c30d40c00a6b8b64c45f3c662ab6cc92b73b48
DavidCR <dval7788@gmail.com>
2018-03-29 10:37:22 -0600

    Cambiada la variable DATABASES en P1Reque/settings.py para conectarse a la BD de PostgreSQL. Datos de Conexion: * Motor: PostgreSQL * BD: baratico * User: su_baratico * Password: jagergato

--- P1Reque/settings.py
+++ P1Reque/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'P1Reque.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     'baratico',
+        'USER':     'su_baratico',
+        'PASSWORD': 'jagergato',
+        'HOST':     'localhost',
+        'PORT':     '',
     }
 }
 
