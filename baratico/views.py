6271a420fe4d1dfd2e6bd67581038473cabbbc92
jeesquivel <esquivelsamudio95@gmail.com>
2018-03-29 17:11:40 -0600

    creación de carpetas: Templeates statics

--- baratico/views.py
+++ baratico/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponseForbidden, HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+from baratico.forms import RegisterUserForm
+
+
+class RegisterUserView(CreateView):
+    form_class = RegisterUserForm
+    template_name = "baratico/register.html"
+
+
+    def dispatch(self, request, *args, **kwargs):
+            if request.user.is_authenticated():
+                return HttpResponseForbidden()
+            return super(RegisterUserView,self).dispatch(request,*args,**kwargs)
+
+
+    def form_valid(self, form):
+        user =form.save(commit=False)
+        user.set_password(form.cleaned_data['password'])
+        user.save()
+        return HttpResponse("Usuario registrado")
